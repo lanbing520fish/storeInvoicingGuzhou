6f82ca6eb99d02cb2f3555d606c5b4c624ed44cf
mac <mac@MacBook-Pro.local>
2017-01-20 09:46:38 +0800

    操作日志

---
+++ view/operationLog/operationLog.js
@@ -0,0 +1,59 @@
+angular
+    .module('operationLogModule', ['ui.bootstrap'])
+    .controller('conditionQuery', ['$scope', '$timeout', function($scope, $timeout) {
+        $scope.format = "yyyy年MM月";
+        $scope.conditionQueryForm = {
+            createStartDt: '', //制单日期开始
+            createEndDt: '' //制单日期结束
+        };
+
+        // 时间控件
+        $scope.startDateOptions = {
+            formatYear: 'yy',
+            maxDate: $scope.conditionQueryForm.endTime,
+            startingDay: 1,
+            showWeeks: false
+        };
+        $scope.endDateOptions = {
+            formatYear: 'yy',
+            minDate: $scope.conditionQueryForm.startTime,
+            startingDay: 1,
+            showWeeks: false
+        };
+        $scope.$watch('conditionQueryForm.startDate', function(newValue) {
+            $scope.endDateOptions.minDate = newValue;
+        });
+        $scope.$watch('conditionQueryForm.endDate', function(newValue) {
+            $scope.startDateOptions.maxDate = newValue;
+        });
+
+        $scope.startOpen = function() {
+            $timeout(function() {
+                $scope.startPopupOpened = true;
+            });
+        };
+        $scope.endOpen = function() {
+            $timeout(function() {
+                $scope.endPopupOpened = true;
+            });
+        };
+        $scope.startPopupOpened = false;
+        $scope.endPopupOpened = false;
+
+    }])
+    // 分页控制器
+    .controller('paginationCtrl', ['$scope', '$rootScope', '$log', function($scope, $rootScope, $log) {
+        $scope.$on('pageChange', function() {
+            $scope.currentPage = 1;
+        });
+
+        $scope.maxSize = 10;
+        $scope.setPage = function(pageNo) {
+            $scope.currentPage = pageNo;
+        };
+
+        $scope.pageChanged = function() {
+            $scope.queryTypeFormSubmit($scope.currentPage);
+            $log.log('Page changed to: ' + $scope.currentPage);
+        };
+    }]);
